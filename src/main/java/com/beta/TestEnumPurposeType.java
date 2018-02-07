20a489344226ad289b4f753717629d41b7c691fe
yinyifan <35989360+yinyifan@users.noreply.github.com>
2018-02-07 18:44:23 +0800

    Richard's work

---
+++ src/main/java/com/beta/TestEnumPurposeType.java
@@ -0,0 +1,25 @@
+package com.beta;
+
+import org.junit.Test;
+
+public class TestEnumPurposeType {
+	
+	
+	@Test
+	public void TestPurposeType()
+	{
+		
+
+		PurposeType a=PurposeType.VendorRequest;
+		System.out.println(a);
+		
+		
+		
+		PurposeType b=PurposeType.CompanyRegistrationEmailVerification;
+		System.out.println(b);
+		
+	}
+	
+	
+
+}
